616e4957b7f0af7e47dd4ec3922ad7fd457fc4a7
kafan1986 <debasish1986@gmail.com>
2020-06-24 02:34:58 +0530

    Update train.py

--- train.py
+++ train.py
@@ -142,9 +142,9 @@ if __name__ == '__main__':
     checkpoint = None
     torch.backends.cudnn.benchmark = True
     train_dataset = Landmark("train.json", input_size, True)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=12)
     val_dataset = Landmark("val.json", input_size, False)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=12)
 
     model = Slim()
     model.train()
