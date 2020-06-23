fa4099925aa9a67d32b569590264a264d7e2e38e
kafan1986 <debasish1986@gmail.com>
2020-06-24 04:13:38 +0530

    Update train.py

--- train.py
+++ train.py
@@ -9,15 +9,16 @@ import time
 from utils.consoler import rewrite, next_line
 
 lr_decay_every_epoch = [1, 25, 35, 75, 150]
-lr_value_every_epoch = [0.00001, 0.0001, 0.00005, 0.00001, 0.000001]
+# lr_value_every_epoch = [0.00001, 0.0001, 0.00005, 0.00001, 0.000001]
+lr_value_every_epoch = [0.0001, 0.001, 0.0005, 0.0001, 0.00001]
 weight_decay_factor = 5.e-4
 l2_regularization = weight_decay_factor
 if "win32" in sys.platform:
     input_size = (96, 96)
-    batch_size = 3072
+    batch_size = 4096
 else:
     input_size = (96, 96)
-    batch_size = 3072
+    batch_size = 4096
 
 
 class Metrics:
