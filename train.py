89a5d9bdf7baaafcdd93fc7b35e3c22e3f8102ba
kafan1986 <debasish1986@gmail.com>
2020-06-24 12:52:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -15,10 +15,10 @@ weight_decay_factor = 5.e-4
 l2_regularization = weight_decay_factor
 if "win32" in sys.platform:
     input_size = (96, 96)
-    batch_size = 256
+    batch_size = 128
 else:
     input_size = (96, 96)
-    batch_size = 256
+    batch_size = 128
 
 
 class Metrics:
